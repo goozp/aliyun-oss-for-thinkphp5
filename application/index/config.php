211e9408cc483f938e0a995477fe8c5aae67500f
gzp <gzp@gzpblog.com>
2017-03-30 21:30:47 +0800

    config

--- application/index/config.php
+++ application/index/config.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'aliyun-oss' => array(
-        'OSS_ACCESS_ID'   => 'LTAIKaqfjHMFCTnU',
-        'OSS_ACCESS_KEY'  => 'dcw7zivdMJjQ8vNlmBQmDNLUfaeZzi',
-        'OSS_ENDPOINT'    => 'http://sgy-test.oss-cn-shenzhen.aliyuncs.com',
-        'OSS_BUCKET'      => 'sgy-test',
+        'OSS_ACCESS_ID'   => '',
+        'OSS_ACCESS_KEY'  => '',
+        'OSS_ENDPOINT'    => '', //例如http://sgy-test.oss-cn-shenzhen.aliyuncs.com
+        'OSS_BUCKET'      => '', //sgy-test
         'OSS_CALLBACKURL' => '', //如使用回调功能，此处配置回调函数的url
     ),
 ];
